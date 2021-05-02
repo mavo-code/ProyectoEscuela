0e4242b842ad0c1b31323faadb4b4d9c06f60e17
Oscar Maldonado Vazquez <OscarMaldonadoVazquez@outlook.com>
2021-05-02 12:09:29 -0500

    creo el proyecto de escuela para el curso de platzi

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace proyecto_escuela
+{
+    
+    class clsEscuela
+    {
+        public string nombreEscuela { get; set; }
+
+        public string direccion { get; set; }
+
+        public int añoFundacion { get; set; }
+
+        public void timbrar()
+        {
+            Console.Beep(1000, 3000);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsEscuela _escuela = new clsEscuela();
+
+            _escuela.timbrar();
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
