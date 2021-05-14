6016ee3c3c87c91b1806da1a7afbd864cac2e9c1
Oscar Maldonado Vazquez <OscarMaldonadoVazquez@outlook.com>
2021-05-14 08:43:05 -0500

    Se generan alumnos de forma aleatoria y listas de asignaturas

--- App/EscuelaEngine.cs
+++ App/EscuelaEngine.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using proyecto_escuela.Entidades;
 
 namespace proyecto_escuela.App
@@ -18,7 +19,6 @@ namespace proyecto_escuela.App
 
             cargarCursos();
             cargarAsignaturas();
-            cargarAlumnos();
             cargarEvaluaciones();
 
         }
@@ -28,16 +28,46 @@ namespace proyecto_escuela.App
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Carga la lista de asignaturas
+        /// </summary>
         private void cargarAsignaturas()
         {
-            throw new NotImplementedException();
+            foreach (var _curso in _escuela.cursos)
+            {
+                List<Asignatura> listaAsignaturas = new List<Asignatura>()
+                {
+                    new Asignatura(){nombre = "Matematicas"},
+                    new Asignatura(){nombre = "Educacion Fisica"},
+                    new Asignatura(){nombre = "Castellano"},
+                    new Asignatura(){nombre = "Ciencias Naturales"},
+                    new Asignatura(){nombre = "Historia"}
+                };
+                _curso.asignaturtas = listaAsignaturas;
+            }
         }
 
-        private void cargarAlumnos()
+        /// <summary>
+        /// Carga la lista de los ALumnos de forma aleatoria
+        /// </summary>
+        /// <returns></returns>
+        private List<Alumno> generarAlumnosAleatorios(int cantidadAlumnos)
         {
-            throw new NotImplementedException();
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+
+            var _listaAlumnos = from n1 in nombre1
+                                from n2 in nombre2
+                                from a1 in apellido1
+                                select new Alumno{nombre=$"{n1} {n2} {a1}"};
+
+            return _listaAlumnos.OrderBy((alumno)=>alumno.uniqueId).Take(cantidadAlumnos).ToList();
         }
 
+        /// <summary>
+        /// Carga la lista de cursos
+        /// </summary>
         private void cargarCursos()
         {
             _escuela.cursos = new List<Curso>(){
@@ -45,8 +75,16 @@ namespace proyecto_escuela.App
                 new Curso(){nombre = "201", jornada = TiposJornada.Mañana},
                 new Curso(){nombre = "301", jornada = TiposJornada.Mañana},
                 new Curso(){nombre = "401", jornada = TiposJornada.Tarde},
-                new Curso(){nombre = "501", jornada = TiposJornada.Tarde},
+                new Curso(){nombre = "501", jornada = TiposJornada.Tarde}
             };
+
+            Random _random = new Random();
+
+            foreach (var _curso in _escuela.cursos)
+            {
+                int cantidadRandom = _random.Next(5, 20);
+                _curso.alumnos = generarAlumnosAleatorios(cantidadRandom);
+            }
         }
     }
 }
